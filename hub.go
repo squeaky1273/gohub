777f97801e7253d6a657e79122b36e81598793e5
squeaky1273 <pjriddell22@gmail.com>
2020-07-15 19:58:25 -0700

    [got info to show up in file]

--- hub.go
+++ hub.go
@@ -1,57 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"encoding/json"
-	"io/ioutil"
-	"os"
-	"log"
-	"github.com/gocolly/colly"
+ "fmt"
+ "encoding/json"
+ "io/ioutil"
+ "os"
+ "log"
+ "github.com/gocolly/colly"
+ "strings"
 )
 
 type Repo struct {
-	Name 	      string  
-	Description   string  
-	Stars         string
-	Users         string
+	Name          string  `json:"name"`
+	Description   string  `json:"description"`
 }
 
-type Repos struct {
-	Repos string
-}
-
-func jsonRepo(filename string, e Repos) {
-	jsonRepo, err := json.Marshal(e)
+func createJsonFile(filename string, repos []Repo) {
+	jsonData, err := json.MarshalIndent(repos, "", "    ")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := ioutil.WriteFile("output.json", jsonRepo, os.ModePerm); err != nil {
+	if err := ioutil.WriteFile(filename, jsonData, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
 	c := colly.NewCollector()
-	var repos [] Repos
+	repos := make([]Repo, 0)
 
 	// On every a element which has title attribute call callback
 	c.OnHTML("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n)", func(e *colly.HTMLElement) {
-		// e.ForEach("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n) > h1", func(e *colly.HTMLElement) {
-		// })
-
-		// e.ForEach("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n) > p", func(e *colly.HTMLElement) {
-		// })
-		repo := Repos{Repos: e.Text}
-
-		j, _ := json.Marshal(repo)
-		_ = ioutil.WriteFile("output.json", j, os.ModePerm)
-
-		for _, repo := range repos {
-			jsonRepo("output.json", repo)
-		}
-
+		// TODO: Clean up data before assigning to struct
+		repo := Repo{}
+		repo.Name = e.ChildText("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n) > h1")
+		repo.Description = e.ChildText("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n) > p")
+		repos = append(repos, repo)
+	})
+	
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+		createJsonFile("results.json", repos)
 	})
 
 	// Before making a request print "Visiting ..."
