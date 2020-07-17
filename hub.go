7692a54d85ec68e0a2d00188d3a65764d6442fb3
squeaky1273 <pjriddell22@gmail.com>
2020-07-17 02:09:48 -0700

    [added comments]

--- hub.go
+++ hub.go
@@ -10,11 +10,13 @@ import (
  "strings"
 )
 
+// Repo struct represents what info will be scraped from github trending page
 type Repo struct {
 	Name          string  `json:"name"`
 	Description   string  `json:"description"`
 }
 
+// Function that creates and writes info to the json file
 func createJsonFile(filename string, repos []Repo) {
 	jsonData, err := json.MarshalIndent(repos, "", "    ")
 
@@ -27,14 +29,16 @@ func createJsonFile(filename string, repos []Repo) {
 	}
 }
 
+// Function that scrpaes name and description from github trending page
 func scrapeHandler() {
 	c := colly.NewCollector()
 	repos := make([]Repo, 0)
 
 	// On every a element which has title attribute call callback
 	c.OnHTML("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n)", func(e *colly.HTMLElement) {
-		// TODO: Clean up data before assigning to struct
+		// Clean up data before assigning to struct
 		repo := Repo{}
+		
 		info := e.ChildText("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n) > h1")
 		name := strings.Replace(info, "\n\n\n     ", "", -1)
 		repo.Name = name
@@ -43,6 +47,7 @@ func scrapeHandler() {
 		repos = append(repos, repo)
 	})
 
+	// Before finishing a scrape print "Finished ..."
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished", r.Request.URL)
 		createJsonFile("results.json", repos)
@@ -53,17 +58,11 @@ func scrapeHandler() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	// Visit the trending go page
 	c.Visit("https://github.com/trending/go?since=daily")
 }
 
+// Function that runs everything
 func main() {
 	scrapeHandler()
 }
-
-		// // usernameAndRepoName := strings.Split(repo.Name, "/")
-		// // username := strings.TrimSpace(usernameAndRepoName[0])
-		// // repoName := strings.TrimSpace(usernameAndRepoName[1])
-		// // repo.Name = username + "/" + repoName
-		// // cleanRepoName := strings.Replace(repo.Name, `\n\n\n\n`, "", -1)
-		// repo.Name = strings.TrimSpace(e.ChildText("body > div.application-main > main > div.explore-pjax-container.container-lg.p-responsive.pt-6 > div > div:nth-child(2) > article:nth-child(n) > h1"))
-		// // repo.Name = strings.Replace(e.Text, `\n\n\n`, "      ", -1)
